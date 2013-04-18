eacfb570c208f6cde1077693b7a6079f7dfa764a
azzamallow <azzamallow@gmail.com>
2013-04-18 12:23:43 +1000

    phonegap automatically downloads

--- ios/index.js
+++ ios/index.js
@@ -2,8 +2,11 @@
 var path = require('path');
 var util = require('util');
 var yeoman = require('yeoman-generator');
+var downloader = require('downloader');
+var AdmZip = require('adm-zip');
 
 var sys = require('sys')
+var fs = require('fs')
 var exec = require('child_process').exec;
 
 module.exports = Generator;
@@ -23,10 +26,6 @@ Generator.prototype.askFor = function askFor() {
     name: 'packageName',
     message: 'What would you like the package name to be?',
     default: 'com.awesome.package'
-  }, {
-    name: 'phonegap',
-    message: 'Where is phonegap installed?',
-    default: '~/Downloads/phonegap-2.6.0'
   }];
 
   this.prompt(prompts, function (err, props) {
@@ -35,20 +34,64 @@ Generator.prototype.askFor = function askFor() {
     }
 
     this.packageName = props.packageName;
-    this.phonegap = props.phonegap;
 
     cb();
   }.bind(this));
+};
+
+Generator.prototype.downloadPhonegap = function downloadPhonegap() {
+
+  var unzip = function () {
+    if (fs.existsSync("/tmp/phonegap-2.6.0/")) {
+      console.log('already extracted');
+      return;
+    }
+    var zip = new AdmZip("/tmp/phonegap-2.6.0.zip");
+    zip.extractAllTo("/tmp", true);
+    fs.chmodSync('/tmp/phonegap-2.6.0/lib/ios/bin/create', '0766');
+    fs.chmodSync('/tmp/phonegap-2.6.0/lib/ios/bin/replaces', '0766');
+    fs.chmodSync('/tmp/phonegap-2.6.0/lib/ios/bin/update_cordova_subprojec', '0766');
+    update_cordova_subprojec
+  };
+
+  var downloadDir = '/tmp/';
+
+  downloader.on('done', function(msg) {
+    console.log('generator-phonegap extracting phonegap...');
+    unzip();
+  });
+
+  downloader.on('error', function(msg) {
+    console.log(msg);
+  });
+
+  if (fs.existsSync("/tmp/phonegap-2.6.0.zip")) {
+    console.log('already downloaded');
+    unzip();
+    return;
+  }
+
+  console.log('generator-phonegap http GET https://s3.amazonaws.com/phonegap.download/phonegap-2.6.0.zip');
+  downloader.download("https://s3.amazonaws.com/phonegap.download/phonegap-2.6.0.zip", downloadDir);
 };
 
 Generator.prototype.generateApp = function generateApp() {
   function puts(error, stdout, stderr) { 
     if (error) {
       sys.puts(stdout); 
+      sys.puts(stderr); 
+      sys.puts(error); 
     } else {
       sys.puts('Success!'); 
     }
   }
 
-  exec(this.phonegap + '/lib/ios/bin/create ./mobile/ios/ ' + this.packageName + ' ' + this.appname, puts);
+  console.log('creating phonegap app');
+  if (!fs.existsSync('./mobile')) {
+    fs.mkdirSync('./mobile');
+  }
+  if (!fs.existsSync('./mobile/ios')) {
+    fs.mkdirSync('./mobile/ios');
+  }
+  exec('/tmp/phonegap-2.6.0/lib/ios/bin/create ./mobile/ios/ ' + this.packageName + ' ' + this.appname, puts);
 };
